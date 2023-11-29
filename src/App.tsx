73947bf1e299145a60adce48093f47dd62a64a7e
Vasanth Srivatsa <vsnthdev@gmail.com>
2023-11-29 21:21:20 +0530

    ✨ create base layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,42 @@
 import { Outlet } from 'react-router-dom'
 
 export function App() {
-    return <>
-        <Outlet />
-    </>
+    return <div className='flex grow'>
+        <section className='flex flex-col justify-between px-2 py-4'>
+            {/* the logo */}
+            <div className='flex flex-col items-center'>
+                <div className='w-8 aspect-square bg-slate-400 rounded-full' />
+            </div>
+
+            {/* nav section */}
+            <nav className='flex flex-col items-center'>
+                <a href="">torrents</a>
+            </nav>
+
+            {/* settings section */}
+            <div className='flex flex-col items-center'>
+                <a href="">dark</a>
+            </div>
+        </section>
+
+        <div className='flex flex-col grow pr-10'>
+            <header className='py-4 flex'>
+                {/* search */}
+                <div className='flex grow'>
+                    <div className='grow border-2 border-black'>
+                        <p>search torrents</p>
+                    </div>
+                </div>
+
+                {/* helpful links */}
+                <div className='flex'>
+                    <a href="">github</a>
+                </div>
+            </header>
+
+            <main className='bg-white rounded-3xl p-10 dark:bg-neutral-800'>
+                <Outlet />
+            </main>
+        </div>
+    </div>
 }
