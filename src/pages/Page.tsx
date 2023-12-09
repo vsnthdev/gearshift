b9e10933276624cdc6cb043547338b85d1753c07
Vasanth Srivatsa <vsnthdev@gmail.com>
2023-12-10 01:46:48 +0530

    🎊 functional new torrent button

--- src/pages/Page.tsx
+++ src/pages/Page.tsx
@@ -6,6 +6,7 @@ import { Transmission } from '@ctrl/transmission'
 import { useTorrents } from '../hooks/useTorrents'
 import { DownloadCloudIcon, HardDriveIcon, UploadCloudIcon } from 'lucide-react'
 import { NewMagnetDetectedDialog, useNewMagnetDetected } from './NewMagnetDetectedDialog'
+import { NewTorrent } from './NewTorrent'
 
 const client = new Transmission({
     baseUrl: `http://192.168.0.100:9091`,
@@ -47,19 +48,7 @@ export function Index() {
                     icon={<HardDriveIcon />}
                 />
 
-                {/* <div className='hidden col-span-1 relative md:aspect-square bg-white rounded-2xl text-center lg:flex flex-col space-y-2 justify-center items-center dark:bg-neutral-900'>
-                    <div className='flex justify-center items-center'>
-                        <div className='p-3 flex bg-black/5 rounded-full dark:bg-white/10'>
-                            <File className='w-5 h-5 dark:text-neutral-300' />
-                        </div>
-                    </div>
-                    <div className='px-4'>
-                        <h4 className='font-semibold'>Drag & drop torrent files here or</h4>
-                    </div>
-                    <div className='flex'>
-                        <button className='text-xs font-medium px-9 py-2 rounded-full transition-colors bg-slate-100 hover:bg-slate-200 dark:bg-neutral-950 dark:hover:bg-neutral-800'>Browse</button>
-                    </div>
-                </div> */}
+                <NewTorrent client={client} />
             </div>
 
             {/* torrents list */}
--- vite.config.ts
+++ vite.config.ts
@@ -24,7 +24,7 @@ export default defineConfig({
             },
             plugins: [
                 NodeGlobalsPolyfillPlugin({
-                    buffer: true
+                    buffer: true,
                 })
             ]
         }
