a4be52f3d3acaecfb7d0472392d0bc41e68d0e21
Vasanth Srivatsa <vsnthdev@gmail.com>
2024-07-25 22:45:43 +0530

    🐛 fix Buffer not found in production

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import Unfonts from 'unplugin-fonts/vite'
 import react from '@vitejs/plugin-react-swc'
-import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill'
+import { nodePolyfills } from 'vite-plugin-node-polyfills'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -10,23 +10,22 @@ export default defineConfig({
             define: {
                 global: 'globalThis'
             },
-            plugins: [
-                NodeGlobalsPolyfillPlugin({
-                    buffer: true,
-                })
-            ]
         }
     },
-    plugins: [react(), Unfonts({
-        google: {
-            display: 'swap',
-            preconnect: true,
-            injectTo: 'head-prepend',
-            families: [{
-                defer: true,
-                name: 'Plus Jakarta Sans',
-                styles: 'ital,wght@0,400;0,500;0,600;0,700;1,400;1,500;1,600;1,700',
-            }],
-        }
-    })]
+    plugins: [
+        react(),
+        Unfonts({
+            google: {
+                display: 'swap',
+                preconnect: true,
+                injectTo: 'head-prepend',
+                families: [{
+                    defer: true,
+                    name: 'Plus Jakarta Sans',
+                    styles: 'ital,wght@0,400;0,500;0,600;0,700;1,400;1,500;1,600;1,700',
+                }],
+            }
+        }),
+        nodePolyfills(),
+    ]
 })
